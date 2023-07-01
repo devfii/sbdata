fbd9c0ed9fc9ab58f0fb4fbe51fcefdeb36a316a
Emmanuel Koomson <devfeefe@gmail.com>
2023-07-01 21:44:58 +0000

    first commit

---
+++ src/main/java/com/devfii/sbdata/SbdataApplication.java
@@ -0,0 +1,13 @@
+package com.devfii.sbdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfii/sbdata/SbdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfii.sbdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
